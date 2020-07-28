5374f0b54c8607b9617dc2fa32d9afc7c501b647
shubham kumar verma <34179123+shubham-skv@users.noreply.github.com>
2020-07-29 01:43:21 +0530

    Add files via upload

---
+++ main.f95
@@ -0,0 +1,15 @@
+program triangle_area
+    implicit none
+
+    real :: average,total
+    integer ::tot
+    logical ::done
+    character(len=80):: message
+    complex ::cx
+    average=1660.5
+    done =.true.
+    tot=565
+    message =" a big hello from shubham kumar verma"
+    print*,tot
+    print *,"this is a complex number",cx
+end program triangle_area
